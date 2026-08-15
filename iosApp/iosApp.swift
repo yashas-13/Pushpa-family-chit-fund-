2a4d621cb47d997f9e1ab3d3315359635130465d
Yashas <167518576+yashas-13@users.noreply.github.com>
2026-08-15 11:12:37 +0530

    feat: wire ios push notification lifecycle

--- iosApp/iosApp.swift
+++ iosApp/iosApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct PushpaFamilyChitApp: App {
+    @UIApplicationDelegateAdaptor(PushNotificationService.self) private var pushService
+
     var body: some Scene {
         WindowGroup {
             ContentView()
