1bd83f9fa084f995be9ac5b3aeeb326f2fe3aaea
Yashas <167518576+yashas-13@users.noreply.github.com>
2026-08-15 11:09:53 +0530

    feat: add android material theme

---
+++ androidApp/src/main/java/in/pravidh/pushpachit/ui/theme/Theme.kt
@@ -0,0 +1,13 @@
+package in.pravidh.pushpachit.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PushpaTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = lightColorScheme(),
+        content = content,
+    )
+}
